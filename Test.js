4c8416e9e3e5c402a77904c7460fc11990e494fa
JENISH_CHOVATIYA <104880911+jenish2214@users.noreply.github.com>
2024-01-01 13:15:24 +0530

    javascript veriable

---
+++ Test.js
@@ -0,0 +1,11 @@
+
+  
+const  accountId =224328 //const value not change his assined by one time 
+let    accountName ="dishant" // use most of let in java script
+// only use veriable diclaration time use " const and var "" veriable
+var   accountBalance = 13443542.87 // issue in block scoop not use 
+const   city ="surat"
+
+console.log(accountName);
+
+console.table([accountId,accountName,accountBalance,city])
